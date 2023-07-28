3a0082a4f2f5eeb6a66b774a115646c436570ff4
Covenant Junior <covenant@gmail.com>
2023-07-29 00:30:50 +0100

    Rename

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Sensor from './Sensor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Sensor />
+    <App />
   </React.StrictMode>
 );
 
