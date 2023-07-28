4021c0d9cf79009e19561cddb570dd83c641077e
Covenant Junior <covenant@gmail.com>
2023-07-29 00:39:27 +0100

    Remove Sensor import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Sensor from './Sensor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
